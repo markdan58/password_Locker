a05b705d1175b029f377c99d198e1ddc00d6138f
markdan58 <mark.dan@strathmore.edu>
2018-11-05 11:35:13 +0300

    Imported the unittest,register class and pyperclip

---
+++ test_register.py
@@ -0,0 +1,13 @@
+import unittest # Importing the unittest module
+from register import Register # Importing the Register class
+import pyperclip
+
+
+class TestRegister(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
