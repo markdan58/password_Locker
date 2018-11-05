57c439652215809e9658296f6f9da87b426c4b03
markdan58 <mark.dan@strathmore.edu>
2018-11-05 11:55:42 +0300

    created a new acccount details

--- login.py
+++ login.py
@@ -2,3 +2,10 @@
 #!/usr/bin/env python3.6
 
 from  register import Register
+
+def create_register(first_name,last_name,username,number,email,password,account_type):
+    '''
+    Function to create a new account
+    '''
+    new_register = Register (first_name,last_name,username,number,email,password,account_type)
+    return new_register
