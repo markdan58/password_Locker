6dd6d2ddd2f7af9e5fccec48e193a8e52afd7f0c
markdan58 <mark.dan@strathmore.edu>
2018-11-05 11:36:48 +0300

    Tested if our objects are being initiated correctly

--- test_register.py
+++ test_register.py
@@ -11,3 +11,24 @@ class TestRegister(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    # 1st Test is Testing if our objects are being initiated correctly
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_Register = Register("James","Muriuki","Jemo-Mruks","0712345678","james@ms.com","jMur463463","Instagram") # create register object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_Register.first_name,"James")
+        self.assertEqual(self.new_Register.last_name,"Muriuki")
+        self.assertEqual(self.new_Register.username,"Jemo-Mruks")
+        self.assertEqual(self.new_Register.phone_number,"0712345678")
+        self.assertEqual(self.new_Register.email,"james@ms.com")
+        self.assertEqual(self.new_Register.password,"jMur463463")
+        self.assertEqual(self.new_Register.account_type,"Instagram")
